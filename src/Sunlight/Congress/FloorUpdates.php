d84a0b41488485d78e84b6f7136d8609905aef0d
Samantha Quinones <samantha@tembies.com>
2013-09-22 14:13:22 -0400

    Added Congress.FloorUpdates support

--- src/Sunlight/Congress/FloorUpdates.php
+++ src/Sunlight/Congress/FloorUpdates.php
@@ -1,10 +1,10 @@
 <?php
 /**
- * Districts.php - Congress API Wrapper
+ * FloorUpdates.php - Congress API Wrapper
  *
  * The API Wrapper provides an interface to construct Sunlight Foundation Congress API queries.
  *
- * @link           http://sunlightlabs.github.io/congress/districts.html
+ * @link           http://sunlightlabs.github.io/congress/floor_updates.html
  *
  * @author         Samantha Quinones <samantha@tembies.com>
  * @package        Sunlight\Congress
@@ -17,38 +17,7 @@ namespace Sunlight\Congress;
 
 use Sunlight\Congress\Api\ApiWrapper;
 
-class Districts extends ApiWrapper
+class FloorUpdates extends ApiWrapper
 {
-    protected $url = "http://congress.api.sunlightfoundation.com/districts/locate";
-
-    /**
-     * Query the /locate subordinate endpoint
-     *
-     * @link http://sunlightlabs.github.io/congress/districts.html
-     *
-     * @param $location
-     *
-     * @return Api\ApiResponse
-     */
-    public function locate($location)
-    {
-        // Clone the wrapper
-        $districts = clone $this;
-
-        // Create an empty filter
-        $filter = new Filter();
-
-        // If the location is an array, assume a lat/long pair, otherwise a zip code
-        if (is_array($location)) {
-            list($filter["latitude"], $filter["longitude"]) = $location;
-        } else {
-            $filter["zip"] = $location;
-        }
-
-        // Set the filter
-        $districts->filter($filter);
-
-        // Run the find method
-        return $districts->filter($filter)->find();
-    }
+    protected $url = "http://congress.api.sunlightfoundation.com/floor_updates";
 }
